20d793b2d7877a11ac27e2f514d90e893b641575
Stuart Ervine <spervine@gmail.com>
2014-05-12 11:18:28 +0100

    Adding hamcrest. [Se]

---
+++ test/com/github/stuartervine/awssugar/PlaceHolderTest.java
@@ -0,0 +1,14 @@
+package com.github.stuartervine.awssugar;
+
+import org.junit.Test;
+
+import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PlaceHolderTest {
+
+    @Test
+    public void justPass() {
+        assertThat(1, is(1));
+    }
+}
